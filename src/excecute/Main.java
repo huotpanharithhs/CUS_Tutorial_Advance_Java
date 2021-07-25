60be2303d6f183d33b0dcc98e4825e34b228ba7b
panharith <huot.panharith.hs@moeys.gov.kh>
2021-07-25 16:18:45 +0700

    Handle show Class Object.

--- src/excecute/Main.java
+++ src/excecute/Main.java
@@ -1,8 +1,25 @@
 package excecute;
 
+import model.Fruit;
+import util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        //This code show console Hello World!
-        System.out.println("Hello World!");
+        Fruit apple1 = new Fruit("Apple", "Red");
+        Fruit apple2 = new Fruit();
+        apple2.setColor("Green");
+        apple2.setName("Apple");
+
+        apple2 = apple1;
+
+        Fruit banana = new Fruit("Banana", "Green");
+
+        Fruit mango = new Fruit("Mango", "Yellow");
+
+        Util.showMessage(apple2.toString());
+        Util.showMessage(apple1.toString());
+        Util.showMessage(banana.toString());
+        Util.showMessage(mango.toString());
+        Util.showMessage(12.0);
     }
 }
--- src/util/Util.java
+++ src/util/Util.java
@@ -1,4 +1,13 @@
 package util;
 
 public class Util {
+    public static void showMessage(String message) {
+        System.out.println(message);
+    }
+    public static void showMessage(int message) {
+        System.out.println(message);
+    }
+    public static void showMessage(double message) {
+        System.out.println(message);
+    }
 }
